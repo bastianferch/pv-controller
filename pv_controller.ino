0ed7147bc4ed39dd0c2cac46c68d6ccc320bbc9b
Bastian Ferch <bastian.ferch@tuwien.ac.at>
2023-03-19 20:56:48 +0100

    noch nix

---
+++ pv_controller.ino
@@ -0,0 +1,15 @@
+//Funktion zur Spannungsmessung an Batterie und PV
+
+//Funktion zur Leistungsregelung zur Batterieentladung
+
+//Logik zur Messung der Tagelänge
+// die Dauer der Nacht ergibt sich aus der Zeit, die PV Spannung 8000mV unterschreitet (Achtung kurzzeitiges Überschreiten möglich)
+
+//Logik zur Berechnung des Ladezustandes der Batterie
+
+//Entscheidungslogik Programmkern
+// alle zwei Minuten
+// es gibt ein Eingangrelais mit zwei Zuständen, direkt (low) und Batterie laden (high)
+// es gibt ein Ausgangsrelais mit zwei Zuständen, direkt (low) und Batterie entladen (hig) 
+
+// 
