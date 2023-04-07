602db2e5b4db7ed8aeab96e1879c1009942242be
Manfred Ferch <manfred.ferch@outlook.com>
2023-04-07 23:56:16 +0200

    Programm strukturiert

--- pv_controller.ino
+++ pv_controller.ino
@@ -17,12 +17,16 @@ const int channel=0;
 #define MEAS_ON 4 // Measure on 4,20
 #define BAT_U_PIN A6 //  input voltage measurement battery A6
 #define PV_U_PIN A7 // input voltage measurement PV A7 
-#define PV_U_BAT_ON 28000 // voltage to start charinging battery
+#define PV_U_START_CHARGING 28000 // voltage to start charinging battery
 #define BAT_U_MAX 30600 //voltage to charging battery full
-#define BAT_U_MAX_H 30200 //voltage to restart charging battery
+#define BAT_U_MAX_H 30200 //voltage to restart charging battery (Hysteresis)
 #define BAT_U_MIN 28000 //voltage to stop discharging battery emty
-#define BAT_U_MIN_H 29000 //voltage to restart discharing
-#define BAT_LIN 29700 // battery linear voltage to power
+#define BAT_U_MIN_H 29000 //voltage to restart discharing (Hysteresis)
+#define BAT_LIN 29700 // battery linear voltage to power 28V until 29,7V
+#define INVERTER_START_DIODE_U 260 // voltage charging diode discharging starts
+#define INVERTER_START_DIODE_U_H 280 //voltage charging diode discharging stops (Hysteresis)
+#define INVERTER_START_U 280 // voltage pv_u discharging starts
+
 
 unsigned long night_start=0, night_length=0, time_power_check;
 word bat_u, pv_u,battery_power;
@@ -38,42 +42,61 @@ void setup(){
   bat_u = measure_voltage(BAT_U_PIN);
   pv_u = measure_voltage(PV_U_PIN);
   stop_measure_voltage();
+
   estimate_bat_power();
-  if ((pv_u > PV_U_BAT_ON) && (bat_u<BAT_U_MAX)) {
-    relay_on[REL_IN]=true;
-    do_relay_on(REL_IN,true);
+
+  if (pv_u > PV_U_START_CHARGING) {
+    if (bat_u<BAT_U_MAX) {
+      relay_on[REL_IN]=true;
+      do_relay_on(REL_IN,true);
+    }        
   } else if (bat_u>BAT_U_MIN_H){
-    discharge_current=3000;
+    discharge_current=2000;
     start_discharging();
   }  
 }
 
 void loop(){
+
   last_bat_u = bat_u;
   last_pv_u = pv_u;
   start_measure_voltage();
   bat_u = measure_voltage(BAT_U_PIN);
   pv_u = measure_voltage(PV_U_PIN);
   stop_measure_voltage();
-  battery_power=calculate_power_in_battery(battery_power);
-  set_power_of_discharge(1500);
-  relay_on[REL_OUT]=true;
-  do_relay_on(REL_OUT,true);
-  delay(1000);
-  set_power_of_discharge(3000);
-  delay(1000);
-  set_power_of_discharge(6500);
-  delay(1000);
-/*
 
-  //Serial.println("Spannung PV(mV):" + String(pv_u) + "Spannug Bat(mV):" + String(bat_u));
-  //Serial.println("beide Rel off");
-  //check_night_start();
-*/
+  battery_power=calculate_power_in_battery(battery_power);
+
+
+  if (!relay_on[REL_IN] && (bat_u<BAT_U_MAX_H)) {
+    if ((!relay_on[REL_OUT] && (pv_u>PV_U_START_CHARGING)) || (relay_on[REL_OUT] && (pv_u>bat_u))) {
+      relay_on[REL_IN]=true;
+      do_relay_on(REL_IN,true);
+    }
+  }
+  if (relay_on[REL_IN] && ((pv_u<bat_u) || (bat_u> BAT_U_MAX))) {
+    relay_on[REL_IN]=false;
+    do_relay_on(REL_IN,false);
+  }
+  if ( !relay_on[REL_OUT] && (bat_u> BAT_U_MIN_H)) {
+    if ((relay_on[REL_IN] && ((pv_u-bat_u)<INVERTER_START_DIODE_U)) || (!relay_on[REL_IN] && ( pv_u <INVERTER_START_U))) {
+      Leistungsermittlung_Nacht();
+      discharge_current=2000;
+      start_discharging();
+    }
+  }
+  if (relay_on[REL_OUT] && ((bat_u<BAT_U_MIN) || (relay_on[REL_IN] && (pv_u-bat_u)>INVERTER_START_DIODE_U_H))) {
+    relay_on[REL_OUT]=false;
+    do_relay_on(REL_OUT,false);
+  }
+  Leistungssteuerung();
+  check_night_start();
+  check_night_end();
+  Parameterausgabe_Serieller_Monitor();
+  delay(7500); //2 Minuten (Taxtfrequenz reduziert auf 1 Mhz)
 }
 
-// KAPITEL Unterprogramme
-
+// ******* CHAPERT SUBROUTINES SETUP
 void clockspeed() {
 //clock to 1MHz
   CLKPR=0x80,
@@ -89,6 +112,7 @@ void pinmodes() {
 }
 
 void initialize_SPI(){
+  // set the slaveSelectPin as an output
   pinMode (SLAVE_SELECT_PIN, OUTPUT);
   // initialize SPI
   SPI.begin();
@@ -105,9 +129,7 @@ void estimate_bat_power(){
   }
 }
 
-  // set the slaveSelectPin as an output
-
-// Battery and PV voltage measurement function
+// ******* CHAPERT SUBROUTINES Battery and PV voltage measurement
 void start_measure_voltage(){
   digitalWrite(MEAS_ON,1);
   delay(10);
@@ -121,66 +143,11 @@ void stop_measure_voltage(){
   digitalWrite(MEAS_ON,0);
 }
 
-void do_relay_on(byte pin, boolean doit){
- if (doit) {
-    digitalWrite(pin, HIGH);
-  } else {
-    digitalWrite(pin, LOW);
-  }     
-}
-
-
-// Logic to measure day length
-// the duration of the night results from the time the PV voltage falls below 8000mV (Caution: short-term exceeding possible)
-/**
-2 longints für Nachtbeginn
-*/
-
-void check_night_start(){
-  if(last_pv_u > 8000 & pv_u < 8000){
-    night_start = millis();
-  }
-}
-void check_night_end(){
-  if(last_pv_u < 8000 & pv_u > 8000 & (millis() - night_start) > 675000){
-    night_length = millis() - night_start;    
-  }
-}
-
-
-//Battery discharge power control function
-void start_discharging() {
-  set_power_of_discharge(1500);
-    relay_on[REL_OUT]=true;
-    do_relay_on(REL_OUT,true);
-    delay(15000);
-    set_power_of_discharge(discharge_current);
-  
-}
-
-void set_power_of_discharge(int value) { //milliampere
-  value = map(value,0,6500,0,58);
-  digitalPotWrite(0, value);
-}
- 
-void digitalPotWrite(int address, int value) {
-  // take the SS pin low to select the chip:
-  digitalWrite(SLAVE_SELECT_PIN, LOW);
-  delay(50);
-  //  send in the address and value via SPI:
-  SPI.transfer(address);
-  SPI.transfer(value);
-  // take the SS pin high to de-select the chip:
-  delay(50);
-  digitalWrite(SLAVE_SELECT_PIN, HIGH); 
-}
-
-
-
-//Logik zur Berechnung des Ladezustandes der Batterie
+// ******* CHAPERT SUBROUTINE Battery Power Calculation
 long calculate_power_in_battery(long powermah){
   word milliamps = 0, diode_u = 0;
-  diode_u = pv_u - bat_u;
+
+  diode_u = (pv_u - bat_u + last_pv_u - last_bat_u) / 2;
   if (relay_on[REL_IN] && (diode_u>0)){  
     switch (diode_u) {
       case 0 ... 250: 
@@ -214,13 +181,70 @@ long calculate_power_in_battery(long powermah){
 }
 
 
-//Entscheidungslogik Programmkern
-// alle zwei Minuten
-// es gibt ein Eingangrelais mit zwei Zuständen, direkt (low) und Batterie laden (high)
-// es gibt ein Ausgangsrelais mit zwei Zuständen, direkt (low) und Batterie entladen (hig) 
+// ******* CHAPERT SUBROUTINES IO Management and SPI Management DigitalResistance
+void do_relay_on(byte pin, boolean doit){
+ if (doit) {
+    digitalWrite(pin, HIGH);
+  } else {
+    digitalWrite(pin, LOW);
+  }     
+}
 
-// Eingangsrelais schalten wie folgt:
-// wenn aus und UPV > 28V und UBat < 30200 mV, dann ein
+void start_discharging() { //Battery discharge power control function
+  set_power_of_discharge(1500);
+    relay_on[REL_OUT]=true;
+    do_relay_on(REL_OUT,true);
+    delay(15000);
+    set_power_of_discharge(discharge_current);
+  
+}
+
+void set_power_of_discharge(int value) { //milliampere
+  value = map(value,0,6500,0,58);
+  digitalPotWrite(0, value);
+}
+ 
+void digitalPotWrite(int address, int value) {
+  // take the SS pin low to select the chip:
+  digitalWrite(SLAVE_SELECT_PIN, LOW);
+  delay(50);
+  //  send in the address and value via SPI:
+  SPI.transfer(address);
+  SPI.transfer(value);
+  // take the SS pin high to de-select the chip:
+  delay(50);
+  digitalWrite(SLAVE_SELECT_PIN, HIGH); 
+}
+
+
+// ******* CHAPERT SUBROUTINES Night Calulations
+void check_night_start(){
+  if(last_pv_u > 8000 & pv_u < 8000){
+    night_start = millis();
+  }
+}
+void check_night_end(){
+  if(last_pv_u < 8000 & pv_u > 8000 & (millis() - night_start) > 675000){
+    night_length = millis() - night_start;    
+  }
+}
+
+
+// ******* CHAPERT SUBROUTINES Night Power Management
+void Leistungsermittlung_Nacht () {
+  
+}
+
+void Leistungssteuerung () {
+  
+}
+
+// ******* CHAPERT SUBROUTINE Information on seriell Monitor
+void Parameterausgabe_Serieller_Monitor() {
+  //Serial.println("Spannung PV(mV):" + String(pv_u) + "Spannug Bat(mV):" + String(bat_u));
+  //Serial.println("beide Rel off");
+}
+
 
 
 void sleep(){
