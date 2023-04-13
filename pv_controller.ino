9d001c3d32b8e3bad538742e09e6addea2c62c0e
Manfred Ferch <manfred.ferch@outlook.com>
2023-04-13 21:49:31 +0200

    minor changes

--- pv_controller.ino
+++ pv_controller.ino
@@ -21,12 +21,12 @@ const int channel = 0;
 #define BAT_U_MAX 30600              // Voltage charging battery full
 #define BAT_U_SOFT_CHARGE 30450      // Voltage to start soft charge near battery full
 #define BAT_U_MAX_H 30200            // Voltage to restart charging battery (Hysteresis)
-#define BAT_U_MIN 28000              // Voltage to stop discharging battery empty
+#define BAT_U_MIN 28300              // Voltage to stop discharging battery empty
 #define BAT_U_MIN_H 29000            // Voltage to restart discharing (Hysteresis)
 #define BAT_LIN 29700                // Battery linear voltage to power 28V until 29,7V
 #define INVERTER_START_DIODE_U 260   // Voltage charging diode discharging starts
 #define INVERTER_START_DIODE_U_H 295 // Voltage charging diode discharging stops (Hysteresis)
-#define BATTERY_CAPACITY 48000       // 48.000mAH Battery Capacity
+#define BATTERY_CAPACITY 45000       // 48.000mAH Battery Capacity
 #define INVERTER_START_TIME 15000    // Inverter start producing power to grid after 4 min = 15000 * 16 millis /clock frequence 1 Mhz instead of 16 MHz
 #define POWER_BASE_MAX 2500          // Maximum Power needed at base time in mA
 #define PEAK_TIME_PERS 45            // Percentage of Peak time, each night firt x% are peak time, last 100%-x& are base time  
@@ -110,24 +110,24 @@ void loop(){
 
   battery_power = calculate_power_in_battery(battery_power);
 
-  //                                         start battery charging relay?
+  //                                       start battery charging relay when PV has enough power
   if (!relay_on[REL_IN] && bat_u < BAT_U_MAX_H) {   
     if ((!relay_on[REL_OUT] && pv_u > PV_U_START_CHARGING) || (relay_on[REL_OUT] &&  pv_u > bat_u)) {
       relay_on[REL_IN] = true;
       do_relay_on(REL_IN, true);
     }
   }
-//                                         stop battery charging relay?
+//                                         stop battery charging relay when no power from pv or battery voltage greater than maximum
   if (relay_on[REL_IN] && (pv_u < bat_u || bat_u > BAT_U_MAX)) {
     relay_on[REL_IN] = false;
     do_relay_on(REL_IN, false);
     if (bat_u > BAT_U_MAX) {
       battery_power = BATTERY_CAPACITY;
-      relay_on[REL_OUT] = false;        // stop discharging soft charging
+      relay_on[REL_OUT] = false;        // stop discharging for soft charging
       do_relay_on(REL_OUT, false);
     }
   }
-//                                         start discharging relay when pv Voltage under start charing voltage or to do soft charing?
+//                                         start discharging relay when pv Voltage under start charging voltage or do soft charing?
   if ( !relay_on[REL_OUT] && bat_u > BAT_U_MIN_H) {
     if ((relay_on[REL_IN] && ((pv_u - bat_u) < INVERTER_START_DIODE_U || bat_u > BAT_U_SOFT_CHARGE)) || (!relay_on[REL_IN] &&  pv_u <=  PV_U_START_CHARGING)) {
       power_base_peak_calculation();
@@ -135,7 +135,7 @@ void loop(){
       start_discharging(discharge_current);
     }
   }
-//                                         stop discharging relay
+//                                         stop discharging relay when battery voltage less then minimum or when charging and Diode Voltage greater than fixed value so that normal pv is producing
   if (relay_on[REL_OUT]) { 
     if (bat_u < BAT_U_MIN || (relay_on[REL_IN] && (pv_u - bat_u) > INVERTER_START_DIODE_U_H)) {
       relay_on[REL_OUT] = false;
@@ -143,7 +143,7 @@ void loop(){
       if (bat_u < BAT_U_MIN) {
         battery_power = 0;
       }
-    }        
+    }
   }
   peak_base_time_soft_charge_power_controll();
   check_night_start();
@@ -177,23 +177,20 @@ long calculate_power_in_battery(long powermah){
       case 0 ... 250: 
         milliamps = map (diode_u, 0, 250, 100, 300); 
         break;
-      case 251 ... 300:     
-        milliamps = map (diode_u, 251, 300, 400, 1000); 
+      case 251 ... 320:     
+        milliamps = map (diode_u, 251, 320, 350, 1000); 
         break;
-      case 301 ... 315:           
-        milliamps = map (diode_u, 301, 315, 1000, 2000); 
+      case 321 ... 335:           
+        milliamps = map (diode_u, 321, 335, 1000, 2000); 
         break;
-      case 316 ... 335:           
-        milliamps = map (diode_u, 316, 335, 2000, 3000); 
+      case 336 ... 390:           
+        milliamps = map (diode_u, 336, 390, 2000, 8000); 
         break;
-      case 336 ... 350:           
-        milliamps = map (diode_u, 336, 350, 3000, 4000); 
-        break;
-      case 351 ... 405:           
-        milliamps = map (diode_u, 351, 405, 4000, 9000); 
+      case 391 ... 410:           
+        milliamps = map (diode_u, 391, 410, 8000, 9000); 
         break;
       default:           
-        milliamps = map (diode_u, 405, 415, 9000, 10000); 
+        milliamps = map (diode_u, 410, 430, 9000, 11000); 
         break;
     }
   }
